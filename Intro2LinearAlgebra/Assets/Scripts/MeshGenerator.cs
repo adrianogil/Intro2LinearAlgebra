e6598c8d892db1a1bb809fd51062fb0ef5007b0f
Adriano Gil <adrianogil.san@gmail.com>
2018-06-17 08:18:54 -0400

    WIP Mesh Generation demo

---
+++ Intro2LinearAlgebra/Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public abstract class MeshGenerator : MonoBehaviour {
+
+    protected abstract Mesh GenerateMesh();
+
+    public void Start () {
+        UpdateMesh();
+    }
+
+	// Use this for initialization
+	public void UpdateMesh () {
+		
+        Mesh newMesh = GenerateMesh();
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter == null)
+        {
+            meshFilter = gameObject.AddComponent<MeshFilter>();
+        }
+
+        meshFilter.mesh = newMesh;
+	}	
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(MeshGenerator))]
+public class MeshGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    
+        MeshGenerator editorObj = target as MeshGenerator;
+    
+        if (editorObj == null) return;
+        
+        if (GUILayout.Button("Update Mesh"))
+        {
+            editorObj.UpdateMesh();
+        }
+    }
+
+}
+#endif
---
+++ Intro2LinearAlgebra/Assets/Scripts/PlaneGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PlaneGenerator : MeshGenerator {
+
+	protected override Mesh GenerateMesh()
+    {
+        Mesh mesh = new Mesh();
+        
+        mesh.vertices = new Vector3[]
+        {
+            new Vector3(0f, 0f, 1f),
+            new Vector3(0f, 1f, 0f),
+            new Vector3(0f, 1f, 1f),
+        };
+
+        mesh.triangles = new int[]
+        {
+            0, 1, 2
+        };
+
+
+        return mesh;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(PlaneGenerator))]
+public class PlaneGeneratorEditor : MeshGeneratorEditor {
+
+}
+#endif
